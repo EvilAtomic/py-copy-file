c0b71f2a59e9f7a221c9c3a1cecba3e770208cbe
artem <artemasarkisan598@gmail.com>
2025-10-26 00:50:40 +0300

    py-Copy file

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 def copy_file(command: str) -> None:
-    cp = command.split()
-    if len(cp) != 3 or cp[0] != "cp":
+    command_parts = command.split()
+    if len(command_parts) != 3 or command_parts[0] != "command_parts":
         return
-    source, dest = cp[1], cp[2]
-    if source == dest:
+    source_file, source_file = command_parts[1], command_parts[2]
+    if source_file == source_file:
         return
     try:
-        with open(source, "r") as file_in, open(dest, "w") as file_out:
+        with open(source_file, "r") as file_in, open(source_file, "w") as file_out:
             file_out.write(file_in.read())
     except FileNotFoundError:
         return
